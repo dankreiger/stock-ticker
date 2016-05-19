aa305734cc59a17c7b1c4ef2b47c4397457d2655
Dan Kreiger <dan@dankreiger.com>
2016-05-19 12:57:10 +0200

    Remove bang mutator from strip method

--- stockticker.rb
+++ stockticker.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'nokogiri'
 
 begin print "\nPlease enter a company stock symbol: "
-	symbol=gets.chomp.downcase.strip!;symbol.gsub!(/\s+/, ".");dom = Nokogiri::HTML(HTTParty.get("http://www.reuters.com/finance/stocks/overview?symbol=#{symbol}"))
+	symbol=gets.chomp.downcase.strip;symbol.gsub!(/\s+/, ".");dom = Nokogiri::HTML(HTTParty.get("http://www.reuters.com/finance/stocks/overview?symbol=#{symbol}"))
 	puts "\nLookup for: "+dom.css('#sectionTitle h1').text()+"\n\n"+dom.css("#headerQuoteContainer").text().gsub!(/[[:space:]]{2,}/, " | ").strip! ; "Source: Reuters"
 rescue 
 	puts "\nSorry: Entry not recognized."
