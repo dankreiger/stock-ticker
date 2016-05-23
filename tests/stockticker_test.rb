7e229e72b01c1047ba7ff338dfc3299a49c11c41
Dan Kreiger <dan@dankreiger.com>
2016-05-23 23:07:12 +0200

    Add tests and travis yml config

---
+++ tests/stockticker_test.rb
@@ -0,0 +1,19 @@
+require "ticker/stockticker.rb"
+require "test/unit"
+require "pry"
+
+class StockTickerTest < Test::Unit::TestCase
+
+  def test_dax
+    assert_equal("Recon Capital DAX Germany ETF (DAX.OQ)", stock_finder("dax").css('#sectionTitle h1').text())
+  end
+  
+  def test_dji
+    assert(stock_finder(".dji").css('#sectionTitle h1').text().include? "Dow Jones Industrial Average")    
+  end
+
+  def test_dlkjds
+    assert_equal("", stock_finder("dlkjds").css('#sectionTitle h1').text())
+  end  
+
+end
